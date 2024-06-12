7e4b662f9e2a58b729055042ae52668bde5aa1b5
Nahuel Gómez <nahue.code@gmail.com>
2024-06-11 22:03:42 -0300

    Fix lint

--- components/header.tsx
+++ components/header.tsx
@@ -13,8 +13,7 @@ export default function Header() {
       </div>
 
       <p className="text-md text-muted-foreground leading-tight font-light mt-1">
-        {/* {config.bio} */}
-        I'm <span className="text-orange-500 font-medium">Full Stack Developer</span> based in Buenos Aires, Argentina. I have a passion for web development and love to create new things. I'm always looking for new challenges and opportunities to grow as a professional.
+        I am <span className="text-orange-500 font-medium">Full Stack Developer</span> based in Buenos Aires, Argentina. I have a passion for web development and love to create new things. I am always looking for new challenges and opportunities to grow as a professional.
       </p>
     </header>
   );
